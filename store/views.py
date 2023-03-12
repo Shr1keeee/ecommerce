d2e3052c3a6e31a19971cc70829a64ce91a5e363
pronin <watcman35@yandex.ru>
2023-03-12 22:19:08 +0400

    added search-bar

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import *
+
+from django.http.response import JsonResponse
+
 # Create your views here.
 
 def home(request):
@@ -24,7 +27,7 @@ def collectionsview(request, slug):
 def productview(request, cate_slug, prod_slug):
     if(Category.objects.filter(slug=cate_slug, status=0)):
         if(Product.objects.filter(slug=prod_slug, status=0)):
-            products = Product.objects.filter(slug=prod_slug, status=0).first
+            products = Product.objects.filter(slug=prod_slug, status=0).first()
             context = {'products': products}
         else:
             messages.error(request, "No such product found")
@@ -33,3 +36,25 @@ def productview(request, cate_slug, prod_slug):
         messages.error(request, "No such category found")
         return redirect('collections')
     return render(request, "store/products/view.html", context)
+
+def productlistAjax(request):
+    products = Product.objects.filter(status=0).values_list('name', flat=True)
+    productList = list(products)
+
+    return JsonResponse(productList, safe=False)
+
+def searchproduct(request):
+    if request.method == 'POST':
+        searchedterm = request.POST.get('productsearch')
+        if searchedterm == "":
+            return redirect(request.META.get('HTTP_REFERER'))
+        else:
+            product = Product.objects.filter(name__contains=searchedterm).first()
+
+            if product:
+                return redirect('collections/'+product.category.slug+'/'+product.slug)
+            else:
+                messages.info(request, "No product matched your search")
+                return redirect(request.META.get('HTTP_REFERER'))
+
+    return redirect(request.META.get('HTTP_REFERER'))
